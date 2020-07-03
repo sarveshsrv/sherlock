81f2f0d1b7c022b8520ff5ccc0ca3639a1d9b21a
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-07-03 23:41:23 +0530

    Name added

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'My Flutter App',
       home: MyHomePage(),
     );
   }
