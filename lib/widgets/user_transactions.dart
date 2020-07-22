72969e2d5f8e5d6c476e29aebc99150270f393c1
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-07-22 22:09:52 +0530

    User transactions updated

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -27,10 +27,10 @@ class _UserTransactionsState extends State<UserTransactions> {
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
+      id: DateTime.now().toString(),
       title: txTitle,
       amount: txAmount,
       date: DateTime.now(),
-      id: DateTime.now().toString(),
     );
 
     setState(() {
